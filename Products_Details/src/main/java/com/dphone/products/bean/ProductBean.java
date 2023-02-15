157b76d6be92f5b82c11d6e6150927c748801924
Sumit Kulhari <sk4213@srmist.edu.in>
2023-02-15 10:18:52 +0530

    Added Product Bean

---
+++ Products_Details/src/main/java/com/dphone/products/bean/ProductBean.java
@@ -0,0 +1,17 @@
+package com.dphone.products.bean;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+import org.springframework.stereotype.Component;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+@Component
+public class ProductBean {
+    private String productName;
+
+}
