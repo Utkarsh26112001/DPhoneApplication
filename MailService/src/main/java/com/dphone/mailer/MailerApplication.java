2662af4731246bc49f395bf526a35b39ef3fefd4
Sumit Kulhari <sk4213@srmist.edu.in>
2023-02-17 23:26:22 +0530

    Updated Mail Application

--- MailService/src/main/java/com/dphone/mailer/MailerApplication.java
+++ MailService/src/main/java/com/dphone/mailer/MailerApplication.java
@@ -7,6 +7,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class MailerApplication {
     public static void main(String[] args) {
         SpringApplication.run(MailerApplication.class,args);
+
+
         System.out.println("Hello world!");
     }
 }
